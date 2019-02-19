8ea2a3a4155c113890385c5e1d69fe5c1ca8559f
AlejaGonzalezV <aleja.gonzalezv19@gmail.com>
2019-02-19 11:58:57 -0500

    Cambios en el formulario

--- WebApplication1/Models/Cliente.cs
+++ WebApplication1/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,14 +10,34 @@ namespace WebApplication1.Models
     {
 
         public int Id { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
         public string nombre { get; set; }
+
+        [Required]
         public string correo { get; set; }
+
         public string telefono { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
         public string necesidad { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
         public string tipoDeEvento { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
         public string ubicacion { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
         public string fecha { get; set; }
         public int invitados { get; set; }
+
+        [StringLength(500, MinimumLength = 30)]
         public string detalles { get; set; }
 
     }
